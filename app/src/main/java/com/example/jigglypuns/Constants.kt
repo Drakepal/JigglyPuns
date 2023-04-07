eff0a0a54edad2adfebf366cecad057ddabdf4fd
Drakepal <playonplay23@gmail.com>
2023-04-07 13:08:06 -0500

    done with video 2

--- app/src/main/java/com/example/jigglypuns/Constants.kt
+++ app/src/main/java/com/example/jigglypuns/Constants.kt
@@ -16,8 +16,108 @@ object Constants {
             "Zaptos Intolerant",
             1
         )
-
         questionsList.add(que1)
+
+        val que2 = Questions(
+            2,
+            "What Pokemon does Dracula like most?",
+            R.drawable.koffing,
+            "Koffin",
+            "Ivysaur",
+            "Prince Charmander",
+            "Wynaut",
+            1
+        )
+        questionsList.add(que2)
+        val que3 = Questions(
+            3,
+            "Which Pokemon could also be a pirate?",
+            R.drawable.arbok,
+            "Arrrrrr-bok",
+            "Charizard",
+            "Chikorita",
+            "Wynaut",
+            1
+        )
+        questionsList.add(que3)
+        val que4 = Questions(
+            4,
+            "I asked my dad why a grown man would play Pokemon Go? He said, ",
+            R.drawable.wynaut,
+            "Hitmonlee",
+            "Mewtwo",
+            "Wynaut",
+            "Zaptos",
+            3
+        )
+        questionsList.add(que4)
+        val que5 = Questions(
+            5,
+            "Why did the Squirtle cross the ocean?",
+            R.drawable.squirtle,
+            "Because it was there.",
+            "He was hot.",
+            "To get to the other tide.",
+            "To play pika-boo.",
+            3
+        )
+        questionsList.add(que5)
+        val que6 = Questions(
+            6,
+            "Why can't you blindfold a Pokemon?",
+            R.drawable.pikachu,
+            "Because it's going to Pikachu!",
+            "Ekans see clearly now, the rain has gone",
+            "They can't see it time can fly",
+            "Wynaut try",
+            1
+        )
+        questionsList.add(que6)
+        val que7 = Questions(
+            7,
+            "What do you call a Weedle who does stunts on a motorcycle?",
+            R.drawable.weedle,
+            "A LightBulbasaur",
+            "Kakuna Rattata",
+            "The Great Gastly",
+            "Weedle Knievel",
+            4
+        )
+        questionsList.add(que7)
+        val que8 = Questions(
+            8,
+            "What did the judge say when a Skuntank came into the court?",
+            R.drawable.skuntank,
+            "I see you've bitten off more than you can pikachu",
+            "I should've kept my big meowth shut",
+            "Odor in the court",
+            "The Pokeball's in your court",
+            3
+        )
+        questionsList.add(que8)
+        val que9 = Questions(
+            9,
+            "What do you call Meowth's reflection?",
+            R.drawable.meowth,
+            "A copycat",
+            "Keep your Meowth shut",
+            "This is a real cat-has-trophy",
+            "Watch out for the tree, it has bark",
+            1
+        )
+        questionsList.add(que9)
+        val que10 = Questions(
+            10,
+            "What happened when Bulbasaur accidentally hurt himself?",
+            R.drawable.bulbasaur,
+            "He cried",
+            "He was Bulbasore",
+            "He said Wynaut",
+            "He wasn't Drowzee anymore",
+            2
+        )
+
+        questionsList.add(que10)
 
         return questionsList
     }
--- app/src/main/java/com/example/jigglypuns/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/jigglypuns/QuizQuestionsActivity.kt
@@ -2,8 +2,29 @@ package com.example.jigglypuns
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class QuizQuestionsActivity : AppCompatActivity() {
+
+    private var progressBar: ProgressBar? = null
+    private var tvProgress: TextView? = null
+    private var tvQuestion: TextView? = null
+    private var tvImage: ImageView? = null
+    private var tvOptionOne: TextView? = null
+    private var tvOptionTwo: TextView? = null
+    private var tvOptionThree: TextView? = null
+    private var tvOptionFour: TextView? = null
+    private var buttonSubmit: Button? = null
+
+    private var mCurrentPosition: Int = 1
+    private var mQuestionsList: ArrayList<Questions>? = null
+    private var mCorrectAnswers: Int = 0
+    private var mUserName: String? = null
+    private var mSelectedOptionPosition: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
