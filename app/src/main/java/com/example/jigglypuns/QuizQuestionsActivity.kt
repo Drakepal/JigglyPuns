7a7df416be6f004584f5df478f0f0b557a7edaa8
Drakepal <playonplay23@gmail.com>
2023-04-07 13:26:17 -0500

    finished video 3

--- app/src/main/java/com/example/jigglypuns/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/jigglypuns/QuizQuestionsActivity.kt
@@ -1,18 +1,21 @@
 package com.example.jigglypuns
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 
-class QuizQuestionsActivity : AppCompatActivity() {
+class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
 
     private var progressBar: ProgressBar? = null
     private var tvProgress: TextView? = null
     private var tvQuestion: TextView? = null
-    private var tvImage: ImageView? = null
+    private var ivImage: ImageView? = null
     private var tvOptionOne: TextView? = null
     private var tvOptionTwo: TextView? = null
     private var tvOptionThree: TextView? = null
@@ -28,5 +31,128 @@ class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
+
+        setQuestion()
+
+
+        tvOptionOne?.setOnClickListener(this)
+        tvOptionTwo?.setOnClickListener(this)
+        tvOptionThree?.setOnClickListener(this)
+        tvOptionFour?.setOnClickListener(this)
+        buttonSubmit?.setOnClickListener(this)
+
+    }
+
+    private fun setQuestion() {
+        val questions: Questions =
+            mQuestionsList!![mCurrentPosition -1]
+
+        defaultOptionsView()
+
+        progressBar?.progress = mCurrentPosition
+        tvProgress?.text = "$mCurrentPosition" + "/" + progressBar?.max
+
+        tvQuestion?.text = questions.question
+        ivImage?.setImageResource(questions.image)
+        tvOptionOne?.text = questions.optionOne
+        tvOptionTwo?.text = questions.optionTwo
+        tvOptionThree?.text = questions.optionThree
+        tvOptionFour?.text = questions.optionFour
+
+
+    }
+
+    private fun defaultOptionsView() {
+
+        val options = ArrayList<TextView>()
+        tvOptionOne?.let {
+            options.add(0, it)
+        }
+        tvOptionTwo?.let {
+            options.add(1, it)
+        }
+        tvOptionThree?.let {
+            options.add(2, it)
+        }
+        tvOptionFour?.let {
+            options.add(3, it)
+        }
+
+        for (option in options) {
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(
+                this@QuizQuestionsActivity,
+                R.drawable.default_option_border
+            )
+        }
+    }
+
+    override fun onClick(view: View?) {
+            when (view?.id) {
+
+                R.id.tv_option_one -> {
+                    tvOptionOne?.let {
+                        selectedOptionView(it, 1)
+                    }
+                }
+                R.id.tv_option_two -> {
+                    tvOptionTwo?.let {
+                        selectedOptionView(it, 2)
+                    }
+                }
+                R.id.tv_option_three -> {
+                    tvOptionThree?.let {
+                        selectedOptionView(it, 3)
+                    }
+                }
+                R.id.tv_option_four -> {
+                    tvOptionFour?.let {
+                        selectedOptionView(it, 4)
+                    }
+                }
+            }
+    }
+
+    private fun selectedOptionView(tv: TextView, selectedOptionNum: Int) {
+
+        defaultOptionsView()
+
+        mSelectedOptionPosition = selectedOptionNum
+
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
+        tv.background = ContextCompat.getDrawable(
+            this@QuizQuestionsActivity,
+            R.drawable.selected_option_border
+        )
+    }
+
+    private fun answerView(answer: Int, drawableView: Int) {
+
+        when (answer) {
+            1 -> {
+                tvOptionOne?.background = ContextCompat.getDrawable(
+                    this@QuizQuestionsActivity,
+                    drawableView
+                )
+            }
+            2 -> {
+                tvOptionTwo?.background = ContextCompat.getDrawable(
+                    this@QuizQuestionsActivity,
+                    drawableView
+                )
+            }
+            3 -> {
+                tvOptionThree?.background = ContextCompat.getDrawable(
+                    this@QuizQuestionsActivity,
+                    drawableView
+                )
+            }
+            4 -> {
+                tvOptionFour?.background = ContextCompat.getDrawable(
+                    this@QuizQuestionsActivity,
+                    drawableView
+                )
+            }
+        }
     }
 }
